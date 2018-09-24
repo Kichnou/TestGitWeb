7cf95436dd59782fc37d681896b48fb5b0bb92ac
AFPA-PARIS-DI\cdi308 <ignace.kevin@gmail.com>
2018-09-24 09:53:14 +0200

    no message

--- src/java/test/Test2.java
+++ src/java/test/Test2.java
@@ -33,16 +33,7 @@ public class Test2 extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Test2</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Test2 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            //Ceci est aussi un test
         }
     }
 
