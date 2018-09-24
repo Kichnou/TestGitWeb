9a6d5259ac130313f1debf49656c2ed931b1d268
AFPA-PARIS-DI\cdi308 <ignace.kevin@gmail.com>
2018-09-24 09:50:09 +0200

    no message

--- src/java/test/NewServlet.java
+++ src/java/test/NewServlet.java
@@ -34,15 +34,7 @@ public class NewServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            //Ceci est un test
         }
     }
 
